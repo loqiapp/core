e8e6d16c468cfc901af85af1552cd88fe1a8d1d1
Florian BROTTE <contact@fbrotte.fr>
2024-06-30 14:41:18 +0200

    update config

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-console.log('test')
+import Wazo from "@wazo/sdk"
+
+// Fonction pour se connecter
+async function authenticate() {
+    try {
+        const username = "hi@loqi.app"
+        const password = "3c6uXryFu87&ZZDvxe4c"
+        const response = await Wazo.Auth.logIn(username, password)
+
+        const token = response.token
+        console.log("Authentification réussie, token:", token)
+
+        // Vous pouvez maintenant utiliser le token pour faire des requêtes authentifiées
+    } catch (error) {
+        console.error("Erreur d'authentification:", error)
+    }
+}
+
+// Appeler la fonction d'authentification
+authenticate()
